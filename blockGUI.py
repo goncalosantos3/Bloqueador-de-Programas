d72828c017f33c32e5f1b9e4abbbed6952a7d427
goncalosantos3 <a95354@alunos.uminho.pt>
2022-07-18 23:00:02 +0100

    Cenas

--- blockGUI.py
+++ blockGUI.py
@@ -10,14 +10,19 @@ from pyparsing import col
 #   1. Temos que definir um widget (butão, janela, barra, qualquer merda de interação com o user)
 #   2. Depois, temos que inserir o widget criado no ecrã da maneira que quisermos 
 
-def aplicaNovaRestrição(clicks, variables, top):
+def aplicaNovaRestrição(clicks, variables, top, myListBox, clicked0):
     
+    #Variables-> Dias da semana em que a restrição se aplica
+    #Clicks-> As horas de cada dia em que a restrição se aplica
+    #top-> janela secundária a fechar no final
+    #myListBox-> uma lista que mostra todas as restrições ativas
+    #clicked0-> nome do programa ao qual a nova restrição se aplica
+
     if (variables[0].get() == 1):#Todos os dias
         horas = []
         horas.append(clicks[0].get() + ":" + clicks[1].get())
         horas.append(clicks[2].get() + ":" + clicks[3].get())
-        print(horas[0])
-        print(horas[1])
+        myListBox.insert(END, clicked0.get() + " bloqueado todos os dias das " + horas[0] + " até às " + horas[1])
     top.destroy()
 
 
@@ -28,11 +33,11 @@ def ativarOptionMenu(optionMenus, number, var1):
         optionMenus[number+i].configure(state = NORMAL)
         i=i+1
 
-def adicionaRestrição():
+def adicionaRestrição(myListBox):
     top = Toplevel()
     top.title("Bloqueador de programas") 
-    top.iconbitmap('C:/Users/smgon/Phyton/Bloqueador-de-Programas/Sawyer_Pilar.ico')
-    top.geometry("500x500")
+    #top.iconbitmap('C:/Users/smgon/Phyton/Bloqueador-de-Programas/Sawyer_Pilar.ico')
+    top.geometry("900x500")
 
     lab0 = Label(top, text="Programa a restringir:")
     lab0.grid(row=0, column=0)
@@ -124,7 +129,7 @@ def adicionaRestrição():
     
     variables = [var1,var2,var3,var4,var5,var6,var7,var8]
 
-    aplicar = Button(top, text="Aplicar", command=lambda: aplicaNovaRestrição(clicks, variables, top))
+    aplicar = Button(top, text="Aplicar", command=lambda: aplicaNovaRestrição(clicks, variables, top, myListBox, clicked0))
     aplicar.grid(row=10,column=10)
     cancelar = Button(top, text="Cancelar", command=top.destroy)
     cancelar.grid(row=10,column=11)
@@ -135,7 +140,7 @@ def adicionaRestrição():
 def alteraRestrição():
     top = Toplevel()
     top.title("Bloqueador de programas")
-    top.iconbitmap('C:/Users/smgon/Phyton/Bloqueador-de-Programas/Sawyer_Pilar.ico')
+    #top.iconbitmap('C:/Users/smgon/Phyton/Bloqueador-de-Programas/Sawyer_Pilar.ico')
 
     my_label = Label(top, text = "Alterar restrição ao programa " + myListBox.get(ANCHOR) + " :")
     my_label.pack()
@@ -152,8 +157,8 @@ def delete():
 
 root = Tk()
 root.title("Bloqueador de Programas")
-root.iconbitmap('C:/Users/smgon/Phyton/Bloqueador-de-Programas/Sawyer_Pilar.ico')
-root.geometry("500x350")
+#root.iconbitmap('C:/Users/smgon/Phyton/Bloqueador-de-Programas/Sawyer_Pilar.ico')
+root.geometry("650x350")
 #Ícone do canto superior esquerdo
 #root.iconbitmap('@/home/goncalo/Desktop/Python/Bloqueador-de-Programas/Sawyer_Pilar.xbm')
 
@@ -171,23 +176,18 @@ myButton2.grid(row=3,column=0)
 myButton4 = Button(root, text="Alterar uma restrição de acesso a um programa já existente", command=alteraRestrição)
 myButton4.grid(row=4, column=0)
 
-myButton3 = Button(root, text="Criar nova restrição a um novo programa", command=adicionaRestrição)
+myButton3 = Button(root, text="Criar nova restrição a um novo programa", command=lambda: adicionaRestrição(myListBox))
 myButton3.grid(row=5,column=0)
 
 my_frame = Frame(root)
 my_scrollbar = Scrollbar(my_frame, orient=VERTICAL)
 
 #global myListBox
-myListBox = Listbox(my_frame, yscrollcommand=my_scrollbar.set, width=30)
+myListBox = Listbox(my_frame, yscrollcommand=my_scrollbar.set, width=70)
 
 my_scrollbar.config(command=myListBox.yview)
 my_scrollbar.pack(side=RIGHT, fill=Y)
 my_frame.grid(row=2, column=0)
 myListBox.pack()
-
-restricoes = ["A tua mãe", "Aquela gorda", "A tua prima aquela obesa", "A tua avó parece um camião",
-    "1", "2", "3", "4", "5", "6", "7", "8", "9"]
-for item in restricoes:
-    myListBox.insert(END, item)
 
 root.mainloop()
